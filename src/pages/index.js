646f91ce4cdfedbca7cb454f58fa82c17fcfff8a
Jason Hamm <jason.robert.hamm@gmail.com>
2022-01-16 21:18:57 -0500

    test pushing update to netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,10 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Jason Hamm's Website</h1>
+    <h2>Projects</h2>
+    <h2>About</h2>
+    <h2>Tech Stack</h2>
+    <h2>Contact</h2>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
