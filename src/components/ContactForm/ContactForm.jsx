50cd3bded3898606187e6b31300a552454cee528
Jason Hamm <jason.robert.hamm@gmail.com>
2022-01-17 22:59:43 -0500

    Create contact form component
    Other:
    -Delete left over image import from Gatsby starter repo

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+const ContactForm = () => {
+  return (
+    <form
+      method="POST"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      style={{
+        display: `flex`,
+        flexDirection: `column`,
+        maxWidth: `500px`,
+      }}
+    >
+      <label htmlFor="email">Your Email:</label>
+      <input type="email" id="email" name="email" />
+      <label htmlFor="subject">Subject:</label>
+      <input type="text" id="subject" name="subject" />
+      <label htmlFor="body">Body:</label>
+      <textarea
+        id="body"
+        name="body"
+        style={{
+          height: `150px`,
+        }}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default ContactForm
