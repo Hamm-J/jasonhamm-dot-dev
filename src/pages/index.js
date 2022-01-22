f0f594ad851b08a51d6cc51826d3f384cbfee301
Jason Hamm <jason.robert.hamm@gmail.com>
2022-01-21 20:16:15 -0500

    Refactor ContactForm and create common Button
    -Refactor `ContactForm` to use styled components
    -Created a `Button` component in `common` for use throughout site
    
    Other:
    -Comment out Gatsby starter links in `index.js`

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import ContactForm from "../components/ContactForm/ContactForm"
+import ContactForm from "../components/ContactForm/Index"
 import Landing from "../components/Landing/Index"
 import Projects from "../components/Projects/Index"
 import About from "../components/About/Index"
@@ -16,14 +16,13 @@ const IndexPage = () => (
     <Projects></Projects>
     <About></About>
     <Tools></Tools>
-    <h2>Contact</h2>
     <ContactForm></ContactForm>
-    <p>
+    {/* <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
       <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    </p> */}
   </Layout>
 )
 
