4dc1c4b063a528492a8e9045269585fa3e1b4056
Jason Hamm <jason.robert.hamm@gmail.com>
2022-02-04 12:20:49 -0500

    Create cms folder and query markdown for Landing
    -Created directory `src/cms` to store markdown files for querying
    -Queried some placeholder data from a placeholder markdown file `tmp1`
    within `Landing`
    -Configured netlify-cms to point to `src/cms` for markdown file creation

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { LandingContainer, Title, Description } from "./Landing.styled";
+import { graphql, useStaticQuery } from "gatsby";
 
 const Landing = () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      markdownRemark {
+        frontmatter {
+          date
+          slug
+          title
+        }
+      }
+    }
+  `);
+  console.log(data.markdownRemark.frontmatter.title);
   return (
     <LandingContainer>
+      <p>{data.markdownRemark.frontmatter.title}</p>
       <Title>Hi, I'm Jason 👋</Title>
       <Description>
         I'm a frontend software developer, cooking enthusiest, and philosophy
