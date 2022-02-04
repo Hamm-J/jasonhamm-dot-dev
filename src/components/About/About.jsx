9a632af53c997aedaea7ced9ee3f8ab2780d62c2
Jason Hamm <jason.robert.hamm@gmail.com>
2022-02-04 15:29:47 -0500

    Setup querying for About
    -Set up graphql querying about `About`
    -deleted the temp `about` markdown file within `src/cms/about`. Created
    two markdown files for each of the `About` section items.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,35 +8,45 @@ import {
 } from "./About.styled";
 import SectionTitle from "../Common/SectionTitle/Index";
 import Comment from "../Common/Comment/Index";
+import { useStaticQuery, graphql } from "gatsby";
 
 // * Think I will divergent my structure for this section
 // = I think I will just list my education and then lab
 
 const About = () => {
+  const data = useStaticQuery(graphql`
+    query AboutQuery {
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "src/cms/about/" } }
+      ) {
+        edges {
+          node {
+            rawMarkdownBody
+            frontmatter {
+              title
+              position
+              start_year
+              end_year
+            }
+          }
+        }
+      }
+    }
+  `);
+  const { edges } = data.allMarkdownRemark;
   return (
     <AboutContainer>
       <SectionTitle>About</SectionTitle>
       <Description>[GENERAL DESC]After pursuing...</Description>
       <Comment>Learn more about me below... [call to action]</Comment>
-      {/* TODO: map over each experience */}
-      <ExperienceTitle>Cornell University</ExperienceTitle>
-      <Position>Student</Position>
-      <Date>2016-2018</Date>
-      <Description>
-        Originally interested in pursuing a career in medicine and completing a
-        bachulars in biology and the entire pre-med track
-      </Description>
-
-      <ExperienceTitle>
-        Experience and Cognition Lab (Cornell University)
-      </ExperienceTitle>
-      <Position>Lab Manager / Research Assistent</Position>
-      <Date>2019 - present</Date>
-      <Description>
-        Carried out data analysis and modeling, programming and implementation
-        of experiments, data visualization primarily using R, Python,
-        JavaScript, CSS, and HTML
-      </Description>
+      {edges.map((edge, edgeIdx) => (
+        <React.Fragment key={edgeIdx}>
+          <ExperienceTitle>{edge.node.frontmatter.title}</ExperienceTitle>
+          <Position>{edge.node.frontmatter.position}</Position>
+          <Date>{`${edge.node.frontmatter.start_year}-${edge.node.frontmatter.end_year}`}</Date>
+          <Description>{edge.node.rawMarkdownBody}</Description>
+        </React.Fragment>
+      ))}
     </AboutContainer>
   );
 };
