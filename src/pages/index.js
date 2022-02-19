3066d6ac23e8aa6e2c141b921ffd5959f93cad22
Jason Hamm <jason.robert.hamm@gmail.com>
2022-02-19 11:47:53 -0500

    Setup styled components theme
    - Add `ThemeProvider` from `styled-components` into `index`
    - create a `theme` file that exports a JSON object with the themes to
      `index`.
    - Set up theme for `Landing`

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { Link } from "gatsby";
 import ContactForm from "../components/ContactForm/Index";
 import Landing from "../components/Landing/Index";
 import Projects from "../components/Projects/Index";
 import About from "../components/About/Index";
 import Tools from "../components/Tools/Index";
 import { IndexContainer } from "../components/Index.styled";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../theme";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
@@ -13,14 +14,16 @@ import Seo from "../components/seo";
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <Landing></Landing>
-    {/* // TODO: temporarily disable projects until projects are ready */}
-    {/* <Projects></Projects> */}
-    <IndexContainer>
-      <About></About>
-      <Tools></Tools>
-      <ContactForm></ContactForm>
-    </IndexContainer>
+    <ThemeProvider theme={theme}>
+      <Landing></Landing>
+      {/* // TODO: temporarily disable projects until projects are ready */}
+      {/* <Projects></Projects> */}
+      <IndexContainer>
+        <About></About>
+        <Tools></Tools>
+        <ContactForm></ContactForm>
+      </IndexContainer>
+    </ThemeProvider>
   </Layout>
 );
 
---
+++ src/theme.js
@@ -0,0 +1,4 @@
+export const theme = {
+  cranberry: "#952121",
+  fontTitle: "Palatino",
+};
