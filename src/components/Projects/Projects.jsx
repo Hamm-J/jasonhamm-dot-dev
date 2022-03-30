edee09a07e9b59c83918a6537f32155ebd1ccc4c
Jason Hamm <jason.robert.hamm@gmail.com>
2022-03-29 22:50:25 -0400

    Fix the order of projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -27,20 +27,37 @@ const Projects = () => {
   `);
 
   const { edges } = data.allMarkdownRemark;
+  const projectsOrdered = [];
+
+  edges.forEach((item) => {
+    switch (item.node.frontmatter.title) {
+      case "My Website":
+        projectsOrdered[2] = [item.node.frontmatter, item.node.rawMarkdownBody];
+        break;
+      case "Storybook React UI Kit":
+        projectsOrdered[1] = [item.node.frontmatter, item.node.rawMarkdownBody];
+        break;
+      case "Reddit Top Posts Heatmap":
+        projectsOrdered[0] = [item.node.frontmatter, item.node.rawMarkdownBody];
+        break;
+      default:
+      // pass
+    }
+  });
 
   return (
     <ProjectsContainer id="projects">
       <SectionTitle>Projects</SectionTitle>
       <ProjectsWrapper>
-        {edges.map((edge, edgeIdx) => (
+        {projectsOrdered.map((project, projectIdx) => (
           <Project
-            key={edgeIdx}
-            title={edge.node.frontmatter.title}
-            image={edge.node.frontmatter.image}
-            techList={edge.node.frontmatter.tech_list}
-            codeLink={edge.node.frontmatter.code_link}
-            deploymentLink={edge.node.frontmatter.deployment_link}
-            description={edge.node.rawMarkdownBody}
+            key={projectIdx}
+            title={project[0].title}
+            image={project[0].image}
+            techList={project[0].tech_list}
+            codeLink={project[0].code_link}
+            deploymentLink={project[0].deployment_link}
+            description={project[1]}
           />
         ))}
       </ProjectsWrapper>
