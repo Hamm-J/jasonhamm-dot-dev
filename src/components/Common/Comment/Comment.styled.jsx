569280e48225b237a6dd2f8102b58b990f4d8210
Jason Hamm <jason.robert.hamm@gmail.com>
2022-05-08 19:44:35 -0400

    Pass fontBody theme prop to comment

--- src/components/Common/Comment/Comment.styled.jsx
+++ src/components/Common/Comment/Comment.styled.jsx
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const Comment = styled.p`
   margin: 0;
   padding: 0;
-  font-family: Trebuchet MS;
+  font-family: ${(props) => props.theme.fontBody};
   font-size: 20px;
 `;
